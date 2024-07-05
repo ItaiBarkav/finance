4a10d8ad5d1c4bda6ec207644a7a37f14d4fa7a5
Itai Barkav <itaibarkav@gmail.com>
2024-07-05 17:49:33 +0300

    FIx AX

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -1,14 +1,33 @@
 const FUEL = { key: 'FUEL', value: ['פז', 'תפוז', 'אלון'] };
 const HVR = { key: 'HVR', value: ['חבר'] };
-const FOOD = { key: 'FOOD', value: ['חומוס', 'פיצה'] };
+const FOOD = {
+  key: 'FOOD',
+  value: [
+    'חומוס',
+    'פיצה',
+    'CARREFOUR',
+    'השפכטל של רחל',
+    'קונדיטוריה',
+    'ניו דלי',
+    "ג'לטו",
+    'שפע הברון',
+  ],
+};
 const INSURANCE = { key: 'INSURANCE', value: ['הפול', 'הראל'] };
+const CULTURE = { key: 'CULTURE', value: ['התרבות'] };
+const ONLINE = {
+  key: 'ONELINE',
+  value: ['ALIBABA COM', 'ALIEXPRESS', 'AMZN'],
+};
 
-export const CATEGORIES = [FUEL, HVR, FOOD, INSURANCE];
+export const CATEGORIES = [FUEL, HVR, FOOD, INSURANCE, CULTURE, ONLINE];
 
 export enum Color {
   FUEL = '#fcaa1f',
   HVR = '#8fd7f3',
   FOOD = '#79c870',
   INSURANCE = '#d40f8c',
+  CULTURE = '#00a0a0',
+  ONELINE = '#003b68',
   OTHER = '#dfdfdf',
 }
--- src/app/services/raw-data.service.ts
+++ src/app/services/raw-data.service.ts
@@ -12,6 +12,9 @@ export class RawDataService {
   private detailsIndex = 4;
   private debitAmountIndex = 5;
   private dataIndex = 0;
+  private nameIndex = 1;
+  private amountIndex = 2;
+  private typeIndex = 3;
 
   constructor() {}
 
@@ -47,21 +50,41 @@ export class RawDataService {
         (value) => value.includes('לכרטיס') // TODO: use transloco
       )
     ) {
-      this.card = Number(row[0].split(' ').pop()!);
+      this.card = Number(row[0].split(' ').pop());
       this.detailsIndex = 4;
       this.debitAmountIndex = 5;
       this.dataIndex = 0;
+      this.nameIndex = 1;
+      this.amountIndex = 2;
+      this.typeIndex = 3;
     }
 
     if (
       row.find(
-        (value) => value.includes('אמריקן אקספרס') // TODO: use transloco
+        (value) => value.includes('אמריקן אקספרס') || value.includes('מסטרקארד') // TODO: use transloco
       )
     ) {
-      this.card = Number(row[0].split(' ').pop()!);
+      this.card = Number(row[0].split(' ').pop());
       this.detailsIndex = 7;
       this.debitAmountIndex = 4;
       this.dataIndex = 1;
+      this.nameIndex = 1;
+      this.amountIndex = 2;
+      this.typeIndex = 3;
+    }
+
+    if (
+      row.find(
+        (value) => value.includes('max executive') // TODO: use transloco
+      )
+    ) {
+      this.card = Number(row[0].split('-').at(0));
+      this.dataIndex = 2;
+      this.amountIndex = 7;
+      this.typeIndex = 8;
+      this.detailsIndex = 9;
+      this.debitAmountIndex = 5;
+      this.nameIndex = 1;
     }
   }
 
@@ -75,15 +98,34 @@ export class RawDataService {
 
   private buildRows(data: string[][], dataIndex: number): void {
     for (let index = dataIndex; index < data.length - this.dataIndex; index++) {
+      if (data[index][1] === '"סך חיוב בש""ח:"') {
+        continue;
+      }
+
+      if (
+        data[index][0] === 'עסקאות בחו˝ל' ||
+        data[index][0] === 'תאריך רכישה'
+      ) {
+        this.dataIndex = 0;
+        this.nameIndex = 2;
+        this.amountIndex = 3;
+        this.typeIndex = 4;
+        this.detailsIndex = 1;
+        this.debitAmountIndex = 5;
+        continue;
+      }
+
       this.transactions.next([
         {
           card: this.card,
           date: data[index][0],
-          name: data[index][1],
-          amount: Number(data[index][2]),
-          type: data[index][3],
+          name: data[index][this.nameIndex],
+          amount: Number(data[index][this.amountIndex].replace(/\u200E/g, '')),
+          type: data[index][this.typeIndex],
           details: data[index][this.detailsIndex],
-          debitAmount: Number(data[index][this.debitAmountIndex]),
+          debitAmount: Number(
+            data[index][this.debitAmountIndex].replace(/\u200E/g, '')
+          ),
         },
         ...this.transactions.value,
       ]);
